55bf10bbae6443b466106702b2d975e50fd55edc
Gina Lee <ginalee@Ginas-MBP.fios-router.home>
2018-06-07 00:07:27 -0700

    added compatibility, content, media queries

--- js/script.js
+++ js/script.js
@@ -1,31 +1,42 @@
 // Function to get the current month to display on favorites
 (function getMonth() {
-    var months = ['January', 'February', 'March', 'April', 'May', 'June',
-    'July', 'August', 'September', 'October', 'November', 'December'];
+  var months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+  ];
 
-    var date = new Date();
-    var m = months[date.getMonth()];
-    document.getElementById('current-month').innerHTML = m; 
-
+  var date = new Date();
+  var m = months[date.getMonth()];
+  document.getElementById("current-month").innerHTML = m;
 })();
 
 // Function to check to see if element is in viewport
 (function($) {
-    $.fn.isInViewport = function() {
-        var elementTop = $(this).offset().top;
-        var elementBottom = elementTop + $(this).outerHeight();
-        var viewTop = $(window).scrollTop();
-        var viewBottom = viewTop + $(window).height();
-        return elementBottom > viewTop && elementTop < viewBottom;
-    };
+  $.fn.isInViewport = function() {
+    var elementTop = $(this).offset().top;
+    var elementBottom = elementTop + $(this).outerHeight();
+    var viewTop = $(window).scrollTop();
+    var viewBottom = viewTop + $(window).height();
+    return elementBottom > viewTop && elementTop < viewBottom;
+  };
 })(jQuery);
 
 // When each row is in the viewport, add the slide class, to animate it
 $(window).scroll(function(event) {
-    $('.row').each(function(i, el) {
-        var el = $(el);
-        if (el.isInViewport(true)) {
-            el.addClass("slide");
-        }
-    });
+  $(".row").each(function(i, el) {
+    var el = $(el);
+    if (el.isInViewport(true)) {
+      el.addClass("slide");
+    }
+  });
 });
